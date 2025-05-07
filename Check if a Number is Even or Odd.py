b1197d5e4016356a66fa17cec69c22cc23ce1e5b
Vaishnvai Jha <97873197+vaishnavijha98@users.noreply.github.com>
2025-05-07 11:35:52 +0530

    Add files via upload

---
+++ Check if a Number is Even or Odd.py
@@ -0,0 +1,5 @@
+Number = int(input("Enter a Number: "))
+if Number % 2 == 0:
+    print(Number,"is an Even number.")
+else:
+    print(Number,"is an Odd number.")
---
+++ Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,6 @@
+total = 0
+for number in range(1,51):
+    total += number
+print("The sum of numbers from 1 to 50 is:", total)
+
+
